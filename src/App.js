d800d2d0073f6049babe13a041af4bd23728c8a7
LBruner <leandrobruner15@gmail.com>
2022-02-18 08:35:55 -0300

    video 003

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, {useState} from 'react';
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./components/Store/CartProvider";
 
+import './App.css';
+import Button from "./components/UI/Button/Button";
 
 function App() {
-    const [cartIsShown, setCartIsShown] = useState(false);
+    const [showParagraph, setShowParagraph] = useState(false);
 
-    const showCartHandler = () => {
-        setCartIsShown(true)
+    console.log('APP running')
+    
+    const toggleParagraphHandler = ()=>{
+        setShowParagraph(previousState => !previousState)
     }
-
-    const hideCartHandler = () => {
-        setCartIsShown(false)
-    }
-
     return (
-        <CartProvider>
-            {cartIsShown && <Cart onClose={hideCartHandler}/>}
-            <Header onShowCart={showCartHandler} onHideCart={hideCartHandler}/>
-            <main>
-                <Meals/>
-            </main>
-        </CartProvider>
-    )
+        <div className="app">
+            <h1>Hi there!</h1>
+            {showParagraph && <p>New thingy thing</p>}
+            <Button show={toggleParagraphHandler}>Toggle</Button>
+        </div>
+    );
 }
 
 export default App;
