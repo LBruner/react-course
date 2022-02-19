3ac49ba08cb9492f6a3b58dfdfbb93786fc27eb6
LBruner <leandrobruner15@gmail.com>
2022-02-19 19:24:15 -0300

    video 005

--- src/components/User.js
+++ src/components/User.js
@@ -2,9 +2,9 @@ import classes from './User.module.css';
 import {Component} from "react";
 
 class User extends Component {
-  render() {
-      return <li className={classes.user}>{this.props.name}</li>
-  }
+    render() {
+        return <li className={classes.user}>{this.props.name}</li>
+    }
 }
 
 // const User = (props) => {
