5fe621716cd9cca77846d9d153016406aad17176
LBruner <leandrobruner15@gmail.com>
2022-02-19 19:06:36 -0300

    video 004

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React, {useCallback, useState} from 'react';
-
-import './App.css';
-import Button from "./components/UI/Button/Button";
-import Demo from "./Demo/Demo";
+import Users from './components/Users';
 
 function App() {
-    const [showParagraph, setShowParagraph] = useState(false);
-    const [allowToggle, setAllowToggle] = useState(false);
-
-    console.log('APP running')
-
-    const toggleParagraphHandler = useCallback(() => {
-        if (allowToggle)
-            setShowParagraph(previousState => !previousState)
-    }, [allowToggle])
-
-    const allowToggleHandler = () => {
-        setAllowToggle(true);
-    }
-
-    return (<div className="app">
-        <h1>Hi there!</h1>
-        <Demo show={showParagraph}/>
-        <Button show={allowToggleHandler}>Allow Toggle</Button>
-        <Button show={toggleParagraphHandler}>Toggle</Button>
-    </div>);
+  return (
+    <div>
+      <Users />
+    </div>
+  );
 }
 
 export default App;
