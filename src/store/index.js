3a67269cda624f196f3e4b47f55e00676d84d8ff
LBruner <leandrobruner15@gmail.com>
2022-04-14 11:01:57 -0300

    video 11

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,8 @@ import {createStore} from 'redux'
 const counterReducer = (state = {counter: 0}, action) =>{
     if (action.type === 'INCREMENT')
         return {counter: state.counter + 1}
+    if (action.type === 'DECREMENT')
+        return {counter: state.counter - 1}
     
     return state;
 }
